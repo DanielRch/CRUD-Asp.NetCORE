21749be1d2207646da8ff659e8c2eb00d29e90b6
DanielRch <69549252+DanielRch@users.noreply.github.com>
2021-08-18 18:02:40 -0300

    Implementação da View de LOG

---
+++ CrudWeb1/CrudWeb1/Controllers/LogAuditoriasController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CrudWeb1.Data;
+using CrudWeb1.Models;
+
+namespace CrudWeb1.Controllers
+{
+    public class LogAuditoriasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LogAuditoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LogAuditorias
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.LogAuditoria.ToListAsync());
+        }
+
+        
+    }
+}
