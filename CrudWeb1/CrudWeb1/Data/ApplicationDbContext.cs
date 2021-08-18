2323e75c560de59bf848628438e8399f263e81ee
DanielRch <69549252+DanielRch@users.noreply.github.com>
2021-08-18 01:24:48 -0300

    Inicial - CRUD básico 1

---
+++ CrudWeb1/CrudWeb1/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CrudWeb1.Models;
+
+namespace CrudWeb1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Produto> Produto { get; set; }
+        public DbSet<LogAuditoria> LogAuditoria { get; set; }
+    }
+}
